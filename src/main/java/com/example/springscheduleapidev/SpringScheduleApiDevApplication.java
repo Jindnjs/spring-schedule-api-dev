316f9afca16d78906f7b3d3563200417434c63ea
Jindnjs <wlsdnjs0310@naver.com>
2025-05-19 13:13:14 +0900

    :tada: initial commit

---
+++ src/main/java/com/example/springscheduleapidev/SpringScheduleApiDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.springscheduleapidev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScheduleApiDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringScheduleApiDevApplication.class, args);
+    }
+
+}
