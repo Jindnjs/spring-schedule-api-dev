8b67328e1ccfe096d6b969c308322b721213088a
Jindnjs <wlsdnjs0310@naver.com>
2025-05-23 09:44:07 +0900

    :sparkles: feat: User 엔티티 클래스 추가
    기본 3계층 구조 작성

---
+++ src/main/java/com/example/springscheduleapidev/user/User.java
@@ -0,0 +1,25 @@
+package com.example.springscheduleapidev.user;
+
+import com.example.springscheduleapidev.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "user")
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    public User() {
+    }
+}
