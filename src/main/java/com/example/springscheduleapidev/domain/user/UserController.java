ac90d8ec46f0c017e2a344852d402345b0e9c754
Jindnjs <wlsdnjs0310@naver.com>
2025-05-27 04:39:12 +0900

    :sparkles: feat: 유저 공통 응답 수정

--- src/main/java/com/example/springscheduleapidev/domain/user/UserController.java
+++ src/main/java/com/example/springscheduleapidev/domain/user/UserController.java
@@ -1,15 +1,12 @@
 package com.example.springscheduleapidev.domain.user;
 
+import com.example.springscheduleapidev.common.response.BaseResponse;
+import com.example.springscheduleapidev.common.response.ResponseCode;
 import com.example.springscheduleapidev.common.security.LoginInfo;
-import com.example.springscheduleapidev.dto.user.CreateUserRequestDto;
-import com.example.springscheduleapidev.dto.user.LoginRequestDto;
-import com.example.springscheduleapidev.dto.user.UserUpdateRequestDto;
-import com.example.springscheduleapidev.dto.user.LoginResponseDto;
-import com.example.springscheduleapidev.dto.user.UserResponseDto;
+import com.example.springscheduleapidev.dto.user.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -21,60 +18,58 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<UserResponseDto> create(
+    public ResponseEntity<BaseResponse<UserResponseDto>> create(
             @Validated @RequestBody CreateUserRequestDto dto
     ){
         UserResponseDto createdUser = userService.create(dto);
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(createdUser);
+        return ResponseEntity.ok(BaseResponse.response(ResponseCode.CREATED,createdUser));
     }
 
     @GetMapping("/users/{id}")
-    public ResponseEntity<UserResponseDto> getById(
+    public ResponseEntity<BaseResponse<UserResponseDto>> getById(
             @PathVariable Long id
     ){
         UserResponseDto user = userService.getById(id);
-        return ResponseEntity.ok(user);
+        return ResponseEntity.ok(BaseResponse.response(ResponseCode.SUCCESS,user));
 
     }
 
     @PatchMapping("/users/{id}")
-    public ResponseEntity<UserResponseDto> update(
+    public ResponseEntity<BaseResponse<UserResponseDto>> update(
             @PathVariable Long id,
             @Validated @RequestBody UserUpdateRequestDto dto
     ){
         UserResponseDto updatedUser = userService.update(id, dto);
-        return ResponseEntity.ok(updatedUser);
+        return ResponseEntity.ok(BaseResponse.response(ResponseCode.SUCCESS,updatedUser));
     }
 
     @DeleteMapping("/users/{id}")
-    public ResponseEntity<String> delete(
+    public ResponseEntity<BaseResponse<?>> delete(
             @PathVariable Long id
     ){
         userService.delete(id);
-        return ResponseEntity.ok("유저 삭제완료 id: " + id);
+        return ResponseEntity.ok(BaseResponse.response(ResponseCode.SUCCESS,null));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(
+    public ResponseEntity<BaseResponse<LoginResponseDto>> login(
             HttpSession session,
             @Validated @RequestBody LoginRequestDto dto
     ){
         LoginResponseDto loginInfo = userService.login(dto);
         session.setAttribute(LoginInfo.LOGIN_INFO, loginInfo);
-        return ResponseEntity.ok("로그인");
+        return ResponseEntity.ok(BaseResponse.response(ResponseCode.SUCCESS,loginInfo));
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<String> logout(
+    public ResponseEntity<BaseResponse<?>> logout(
             HttpServletRequest request
     ){
         HttpSession session = request.getSession(false);
         if (session != null) {
             session.invalidate();
         }
-        return ResponseEntity.ok("로그아웃");
+        return ResponseEntity.ok(BaseResponse.response(ResponseCode.SUCCESS,null));
     }
 
 }
